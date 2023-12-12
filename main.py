7eac01dc920d9a360b09d5e209aa30c18620c0b2
ewan <71148323+ewanj000@users.noreply.github.com>
2023-12-12 11:07:21 +0000

    created config.json

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from tuyapy import TuyaAPI
 import time
-
+import json
+with open("./config.json","r") as f:
+    data = json.load(f)
+    username = data("username")
+    password = data("password")
+    device_id = data("device_id")
 
 # email to discord code goes above so i dont have to spend ages writing the get email code again
 
