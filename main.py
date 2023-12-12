6fe7d603314399e44a59ad359e9ca9be18386a29
ewan <71148323+ewanj000@users.noreply.github.com>
2023-12-12 10:55:42 +0000

    added basic code with probably wrong syntax bc i dont remember it and im using the vs code web app at school

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from tuyapy import TuyaAPI
+import time
 
+# login to api
+TuyaAPI.login(username, password)
+
+if mail.subject = "Front Door Camera Line Crossing":
+    TuyaAPI.get_device_by_id(device_id).turn_on
+    time.sleep(600)
+    TuyaAPI.get_device_by_id(device_id).turn_off
